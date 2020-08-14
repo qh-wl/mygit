131a827e92dd0d9542bf19e803d874b36de2055a
qh-wl <2749902798@qq.com>
2020-08-14 11:46:48 +0800

    第一次更新

---
+++ src/main/java/com/qh/springcloud_03/Springcloud03Application.java
@@ -0,0 +1,13 @@
+package com.qh.springcloud_03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springcloud03Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springcloud03Application.class, args);
+    }
+
+}
